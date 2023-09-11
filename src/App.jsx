e7518aaccb0bbbf7dbc3c39680ef350b21983b5a
Christopher <csbroox@gmail.com>
2023-09-11 13:34:54 -0500

    about to npmrundev

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,86 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+import {Link, useLocation, Routes, Route, useParams } from 'react-router-dom';
 
-import './App.css'
+const Home = () => {
+  return ( <h1>HOME</h1> );
+};
+
+const Posts = ({ posts }) => {
+  return (
+  <div>
+    <h1>Posts</h1>
+    <ul>
+    {
+      posts.map( post => {
+        return (
+          <li key={ post.id }>
+            { post.name }
+          </li>
+        );
+      })
+    }
+    </ul>
+  </div>
+  );
+}
+
+const Users = ({ users }) => {
+  return (
+  <div>
+    <h1>Users</h1>
+    <ul>
+    {
+      users.map( user => {
+        return (
+          <li key={ user.id }>
+            { user.name }
+          </li>
+        );
+      })
+    }
+    </ul>
+  </div>
+  );
+}
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [users, setUsers] = useState([]);
+  const location = useLocation();
+  const { pathname } = location;
 
+  useEffect(() => {
+    const fetchPosts = async() => {
+      const response = await axios.get('https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/posts');
+      const data = response.data;
+      setPosts(data.data);
+    }
+    fetchPosts();
+  }, [])
+
+  useEffect(() => {
+    const fetchUsers = async() => {
+      const response = await axios.get('https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users');
+      const data = response.data;
+      setUsers(data.data);
+    }
+    fetchUsers();
+  }, [])
 
   return (
     <>
+    <nav>
+      <Link to='/' className={ pathname === '/' ? 'selected': ''}>HOME</Link>
+      <Link to='/posts' className={ pathname === '/posts' ? 'selected': ''}>Posts ({ posts.length })</Link>
+      <Link to='/users' className={ pathname === '/users' ? 'selected': ''}>Posts ({ users.length })</Link>
+    </nav>
+
+    <Routes>
+      <Route path='/' element={ <Home/> } />
+      <Route path='/posts' element={ <Posts posts={ posts }/> } />
+      <Route path='users' element={ <Users users={ users } /> } />
+    </Routes>
       
     </>
   )
